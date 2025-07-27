dc419955ed65a23895b6956963dfce69d4c7034e
nagano <shionaga0214@gmail.com>
2025-07-27 09:52:02 +0900

    判定条件の部分を修正した

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 for (let num = 1; num <= 20; num++) {
-  if (num % 15 === 0) {
+  if (num % 5 === 0 && num % 3 === 0) {
     console.log("FizzBuzz");
   } else if (num % 5 === 0) {
     console.log("Buzz");
