62dc2511bb88d8556ddfaaac117c964f891edcc8
nagano <shionaga0214@gmail.com>
2025-08-08 13:21:11 +0900

    new Date() の呼び出しを変数に1回だけ代入し、その変数から年と月を取得するよう修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,8 +4,9 @@ import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 
-const year = argv.y ?? new Date().getFullYear();
-const month = argv.m ?? new Date().getMonth() + 1;
+const now = new Date();
+const year = argv.y ?? now.getFullYear();
+const month = argv.m ?? now.getMonth() + 1;
 
 const firstDay = new Date(year, month - 1, 1);
 const lastDay = new Date(year, month, 0);
