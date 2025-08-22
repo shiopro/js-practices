256d106223f4f4e5fd90c01140c31fb58fa75b37
nagano <shionaga0214@gmail.com>
2025-08-22 13:12:04 +0900

    Promise化した共通関数を使ってエラーなし処理を実装

---
+++ 03.asynchronous/promise_success.js
@@ -0,0 +1,22 @@
+import { run, get, all, close } from "./db.js";
+
+run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => run("INSERT INTO books (title) VALUES (?)", ["Book"]))
+  .then(() => get("SELECT * FROM books WHERE title = ?", ["Book"]))
+  .then((row) => {
+    console.log("取得:", row);
+    return all("SELECT * FROM books");
+  })
+  .then((rows) => {
+    console.log("全体:", rows);
+  })
+  .catch((error) => {
+    console.error("エラー:", error.message);
+  })
+  .finally(() => {
+    close()
+      .then(() => console.log("DB接続を閉じました"))
+      .catch((error) => console.error("close() エラー:", error));
+  });
