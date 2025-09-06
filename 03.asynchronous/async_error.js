3ac6ad8e36c74282e2cd007ab0de337a3ef819a6
nagano <shionaga0214@gmail.com>
2025-09-06 22:21:40 +0900

    トップレベルawaitを使った書き方に修正

--- 03.asynchronous/async_error.js
+++ 03.asynchronous/async_error.js
@@ -3,35 +3,31 @@ import { run, get, close } from "./db.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function main() {
+try {
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  console.log("テーブル作成成功");
+
   try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("テーブル作成成功");
-
-    try {
-      await run(db, "INSERT INTO books (title) VALUES (?)", [null]);
-    } catch (error) {
-      console.error("レコード追加エラー:", error.message);
-    }
-
-    try {
-      await get(db, "SELECT * FROM users");
-    } catch (error) {
-      console.error("レコード取得エラー:", error.message);
-    }
-
-    try {
-      await run(db, "DROP TABLE books");
-      console.log("テーブル削除成功");
-    } finally {
-      await close(db);
-    }
+    await run(db, "INSERT INTO books (title) VALUES (?)", [null]);
   } catch (error) {
-    console.error("エラー:", error.message);
+    console.error("レコード追加エラー:", error.message);
   }
-}
 
-main();
+  try {
+    await get(db, "SELECT * FROM users");
+  } catch (error) {
+    console.error("レコード取得エラー:", error.message);
+  }
+
+  try {
+    await run(db, "DROP TABLE books");
+    console.log("テーブル削除成功");
+  } finally {
+    await close(db);
+  }
+} catch (error) {
+  console.error("エラー:", error.message);
+}
--- 03.asynchronous/async_success.js
+++ 03.asynchronous/async_success.js
@@ -3,30 +3,24 @@ import { run, get, all, close } from "./db.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function main() {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  console.log("テーブル作成成功");
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+console.log("テーブル作成成功");
 
-  const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
-    "Book",
-  ]);
-  console.log("追加したレコードのID:", result.lastID);
+const result = await run(db, "INSERT INTO books (title) VALUES (?)", ["Book"]);
+console.log("追加したレコードのID:", result.lastID);
 
-  const row = await get(db, "SELECT * FROM books WHERE title = ?", ["Book"]);
-  console.log("取得したレコード:", row);
+const row = await get(db, "SELECT * FROM books WHERE title = ?", ["Book"]);
+console.log("取得したレコード:", row);
 
-  const rows = await all(db, "SELECT * FROM books");
-  console.log("全体のレコード:", rows);
+const rows = await all(db, "SELECT * FROM books");
+console.log("全体のレコード:", rows);
 
-  try {
-    await run(db, "DROP TABLE books");
-    console.log("テーブル削除成功");
-  } finally {
-    await close(db);
-  }
+try {
+  await run(db, "DROP TABLE books");
+  console.log("テーブル削除成功");
+} finally {
+  await close(db);
 }
-
-main();
