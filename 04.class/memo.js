db0b7f939c4af68ba4d820375f24dd57b2f306d5
nagano <shionaga0214@gmail.com>
2025-09-09 09:56:31 +0900

    メモの追加機能を実装

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+import readline from "readline";
 
 const db = new sqlite3.Database("memo.db", (error) => {
   if (error) {
@@ -20,3 +21,44 @@ db.run(
     }
   },
 );
+
+class MemoApp {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async addMemo() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    try {
+      console.log("メモを入力してください(Ctrl+Dで終了):");
+      let lines = [];
+
+      for await (const line of rl) {
+        lines.push(line);
+      }
+      const memo = lines.join("\n");
+
+      await new Promise((resolve, reject) => {
+        db.run("INSERT INTO memos (memo) VALUES (?)", [memo], (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+      console.log("メモを保存成功");
+    } catch (error) {
+      console.error("メモ保存失敗:", error.message);
+    } finally {
+      rl.close();
+    }
+  }
+}
+
+const app = new MemoApp(db);
+await app.addMemo();
