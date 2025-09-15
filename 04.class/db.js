f2891762eb07ba7f3d982fb4bc53d403acceb21a
nagano <shionaga0214@gmail.com>
2025-09-15 10:18:29 +0900

    DB接続完了後にテーブル作成するよう修正

--- 04.class/db.js
+++ 04.class/db.js
@@ -5,19 +5,19 @@ const db = new sqlite3.Database("memo.db", (error) => {
     console.error("DB接続エラー:", error.message);
     process.exit(1);
   }
-});
 
-db.run(
-  `CREATE TABLE IF NOT EXISTS memos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  content TEXT
-  );`,
-  (error) => {
-    if (error) {
-      console.error("テーブル作成失敗:", error.message);
-      process.exit(1);
-    }
-  },
-);
+  db.run(
+    `CREATE TABLE IF NOT EXISTS memos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+    );`,
+    (error) => {
+      if (error) {
+        console.error("テーブル作成失敗:", error.message);
+        process.exit(1);
+      }
+    },
+  );
+});
 
 export default db;
